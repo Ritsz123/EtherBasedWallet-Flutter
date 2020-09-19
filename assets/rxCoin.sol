e178a471f6cd317d45c169ff4df3af1f1bbff4f9
Ritesh Khadse <48131636+Ritsz123@users.noreply.github.com>
2020-09-19 14:16:04 +0530

    added rxCoin.sol

---
+++ assets/rxCoin.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.6.6;
+
+contract RxCoin{
+    int balance;
+    constructor() public{
+        balance = 0;
+    }
+
+    function getBalance() view public returns (int){
+        return balance;
+    }
+
+    function depositBalance(int amount) public {
+        balance = balance + amount;
+    }
+
+    function withdrawBalance(int amount) public {
+        balance = balance - amount;
+    }
+
+}
