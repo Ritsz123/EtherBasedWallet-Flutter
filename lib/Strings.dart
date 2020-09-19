b6278cffc500070a212cf5da37cc8105ad7a5f55
Ritesh Khadse <48131636+Ritsz123@users.noreply.github.com>
2020-09-19 14:17:57 +0530

    Strings.dart

--- lib/Strings.dart
+++ lib/Strings.dart
@@ -1,7 +1,7 @@
-final myAddress = "0x2Ee9aB986c42B8EF5e5261AfE8d42CD0e2E01d0c";
+final myAddress = "0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 final String clientUrl =
-    "https://rinkeby.infura.io/v3/9ba4f038b5854ab5b4051eeecf680045";
-final String contractAddress = "0xc0B347b6260BD1652e9C85B67b749223E6193925";
+    "https://rinkeby.infura.io/v3/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+final String contractAddress = "0xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 final String contractName = "RXCOIN";
 final String privateKey =
-    "7c2fd00b47ca46ccd72a73dfa9cd86ff5237706050f911e9a6a404aacb8a33d4";
+    "yxyxyxyxyxyyxyxyxyxyyxyxyxyxyxyxyyxyxyxyxyyxyxyxyxyyxyxyxyxyx";
